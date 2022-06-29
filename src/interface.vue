d0d40f83368a20cc512cccf0db9e91e82109e3ec
Leo Roettele <leo.roettele@liip.ch>
2022-06-29 16:37:59 +0200

    fix interface

--- src/interface.vue
+++ src/interface.vue
@@ -1,82 +1,69 @@
 <template>
-	<div style="margin: 20px">
-		<v-form
-			:fields="seoFields"
-			:model-value="internalValue"
-			:initial-value="initial"
-			@update:model-value="updateInternalValue($event)"
-		></v-form>
-	</div>
-
-	<v-divider />
+  <v-form
+      :fields="fields"
+      :initial-values="initial"
+      :model-value="{ ...initial, ...(hasValueChanged ? value : {}) }"
+      @update:modelValue="setValue($event)"
+  ></v-form>
 </template>
 
 <script setup lang="ts">
-import { useStores, useApi } from '@directus/extensions-sdk';
-import { computed, ref, watch } from 'vue';
+import { computed, onBeforeUnmount, ref, watch } from 'vue';
+import { useApi, useStores } from '@directus/extensions-sdk';
 
-const props = withDefaults(
-	defineProps<{
-		collection: string;
-		field: string;
-		value: number | Object;
-	}>(),
-	{
-		value: () => [],
-	}
-);
+const props = defineProps({
+  collection: {
+    type: String,
+    required: true,
+  },
+  field: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: [Object, Number],
+    default: [],
+  },
+});
 
+const api = useApi();
 const stores = useStores();
 const fieldsStore = stores.useFieldsStore();
 const relationsStore = stores.useRelationsStore();
 const collectionsStore = stores.useCollectionsStore();
-const api = useApi();
 
 const relatedCollection = collectionsStore.getCollection(
-	relationsStore.getRelationForField(props.collection, props.field).related_collection
+    relationsStore.getRelationForField(props.collection, props.field).related_collection
 );
 
 const emit = defineEmits(['input']);
-const internalValue = computed({
-	get: () => props.value,
-	set: (value) => {
-		emit('input', value);
-	},
+
+const value = computed({
+  get: () => props.value,
+  set: (value) => {
+    emit('input', value);
+  },
 });
 
-const seoFields = computed(() => {
-	if (!relatedCollection) return [];
-	return fieldsStore.getFieldsForCollection(relatedCollection.collection);
-});
+const hasValueChanged = ref(false);
 
-const getSeoValues = async (id) => {
-	const response = await api.get(`/items/${relatedCollection.collection}/${id}`);
-	return response.data?.data || emptyValue.value;
+const setValue = (input) => {
+  hasValueChanged.value = true;
+  value.value = input;
 };
 
-const updateInternalValue = (item, edited = true) => {
-	internalValue.value = { ...emptyValue.value, ...item, edited: edited };
-};
-
-const emptyValue = computed(() => {
-	if (!seoFields.value) return {};
-	return seoFields.value
-		.filter((field) => !field.meta.hidden)
-		.reduce((acc, field) => {
-			acc[field.field] = null;
-			return acc;
-		}, {});
+const fields = computed(() => {
+  if (!relatedCollection) return [];
+  return fieldsStore.getFieldsForCollection(relatedCollection.collection);
 });
 
-const initial = ref(emptyValue.value);
-
-watch(internalValue, async (newValue) => {
-	if (!newValue || Number(newValue) !== newValue) {
-		return;
-	}
-
-	initial.value = await getSeoValues(newValue);
-	// Replace value with the loaded entry
-	updateInternalValue(initial.value);
+// Use ref and watcher, since async computed would return Promise<Object>
+const initial = ref({});
+watch(value, async (value) => {
+  if (typeof value === typeof Number()) {
+    const response = await api.get(`/items/${relatedCollection.collection}?filter[id][_eq]=${value}`);
+    const data = response.data.data;
+    if (data.length === 1) initial.value = data[0];
+  }
 });
 </script>
