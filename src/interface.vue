48bbcc774a102602b30bba9470267cebe0480d47
Leo Roettele <leo.roettele@liip.ch>
2022-09-27 13:41:30 +0200

    Remove old code because directus works now.

--- src/interface.vue
+++ src/interface.vue
@@ -1,14 +1,9 @@
 <template>
-	<v-form
-		:fields="fields"
-		:initial-values="initial"
-		:model-value="{ ...initial, ...(hasValueChanged ? value : {}) }"
-		@update:model-value="setValue($event)"
-	></v-form>
+	<v-form :fields="fields" :initial-values="initial" :model-value="value" @update:model-value="value = $event"></v-form>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { defineProps, defineEmits, computed, ref, watchEffect } from 'vue';
 import { useApi, useStores } from '@directus/extensions-sdk';
 
 const props = defineProps({
@@ -21,10 +16,8 @@ const props = defineProps({
 		required: true,
 	},
 	value: {
-		type: [Object, Number],
-		default() {
-			return [];
-		},
+		type: [Object, Number, null],
+		default: null,
 	},
 });
 
@@ -33,37 +26,28 @@ const stores = useStores();
 const fieldsStore = stores.useFieldsStore();
 const relationsStore = stores.useRelationsStore();
 const collectionsStore = stores.useCollectionsStore();
-
 const relatedCollection = collectionsStore.getCollection(
 	relationsStore.getRelationForField(props.collection, props.field).related_collection
 );
-
-const emit = defineEmits(['input']);
-
-const value = computed({
-	get: () => props.value,
-	set: (value) => {
-		emit('input', value);
-	},
-});
-
-const hasValueChanged = ref(false);
-
-const setValue = (input) => {
-	hasValueChanged.value = true;
-	value.value = input;
-};
-
 const fields = computed(() => {
 	if (!relatedCollection) return [];
 	return fieldsStore.getFieldsForCollection(relatedCollection.collection);
 });
 
-// Use ref and watcher, since async computed would return Promise<Object>
+const emit = defineEmits(['input']);
+const isId = (value) => typeof value === typeof Number();
 const initial = ref({});
-watch(value, async (value) => {
-	if (typeof value === typeof Number()) {
-		const response = await api.get(`/items/${relatedCollection.collection}?filter[id][_eq]=${value}`);
+
+const value = computed({
+	get: () => (isId(props.value) ? initial.value : props.value),
+	set: (value) => {
+		emit('input', value);
+	},
+});
+
+watchEffect(async () => {
+	if (isId(props.value)) {
+		const response = await api.get(`/items/${relatedCollection.collection}?filter[id][_eq]=${props.value}`);
 		const data = response.data.data;
 		if (data.length === 1) initial.value = data[0];
 	}
